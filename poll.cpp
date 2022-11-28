4e198bff4090cd79d67157095b2eb6601684d449
Alex Keener <alexkeener318@gmail.com>
2022-11-28 15:02:45 -0600

    Adds team information

--- poll.cpp
+++ poll.cpp
@@ -1,19 +1,20 @@
 #include <string>
 #include <iostream>
+#include <vector>
 
-using std::string, std::cout, std::cin, std::endl;
+using std::string, std::cout, std::cin, std::endl, std::vector;
 // sort poll
-void getRankings(string* teamNames, int* wins, int* losses, int* difficult, string* rankings){
+void getRankings(vector<string> teamNames, int wins, int losses, int difficulty, vector<string> &rankings){
 
 }
 
 // updateWins/Losses
-void updateWins(string* teamNames, int* wins, int* losses){
+void updateWins(vector<string> teamNames, int* wins, int* losses){
 
 }
 // get input to update teams
 void getInput(){
-    
+
 }
 // 
 
@@ -21,11 +22,14 @@ void getInput(){
 
 
 int main(){
-    string* teamNames[36] = {};
-    int* wins[36] = {};
-    int* losses[36] = {};
-    int* difficulty[36] = {};
+    vector<string> teamNames = {"Alabama","Texas A&M","Auburn","LSU","Georgia","South Carolina","Tennessee","Florida","Vanderbilt","Kentucky",
+                                "Ole Miss","Arkansas","USC","Utah","UCLA","Oregon","Baylor","Texas","Oklahoma","TCU","Texas Tech","Kansas State",
+                                "Kansas","Oklahoma State","Ohio State","Illinois","Michigan","Iowa","Florida State","Clemson","Wake Forest","Louisville"};
+    int wins[32];
+    int losses[32];
+    int difficulty[32] = {10,7,8,9,10,7,9,6,5,6,7,8,9,7,6,8,5,7,7,9,6,8,7,5,10,5,9,4,6,7,3,3};
 
-    string* listing[36] = {};
+    vector<string> ranking[32];
+    int weekNum = 0;
 
 }
