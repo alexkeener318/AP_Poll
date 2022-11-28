17058480b75570958c64e7ccf3f976c7d17dd8c3
Alex Keener <alexkeener318@gmail.com>
2022-11-28 10:54:13 -0600

    Sets up starter file

---
+++ poll.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+
+using std::string, std::cout, std::cin, std::endl;
+// sort poll
+void getRankings(string* teamNames, int* wins, int* losses, int* difficult, string* rankings){
+
+}
+
+// updateWins/Losses
+void updateWins(string* teamNames, int* wins, int* losses){
+
+}
+// get input to update teams
+void getInput(){
+    
+}
+// 
+
+
+
+
+int main(){
+    string* teamNames[36] = {};
+    int* wins[36] = {};
+    int* losses[36] = {};
+    int* difficulty[36] = {};
+
+    string* listing[36] = {};
+
+}
